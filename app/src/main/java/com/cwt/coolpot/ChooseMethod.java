a757ac2e4900801bf3d4f8d2fd3a96656c466b55
曹吵吵 <657822436@qq.com>
2017-08-18 13:07:22 +0800

    first time

---
+++ app/src/main/java/com/cwt/coolpot/ChooseMethod.java
@@ -0,0 +1,13 @@
+package com.cwt.coolpot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseMethod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_method);
+    }
+}
